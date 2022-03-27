c9975dbb799d11f471bc2b10ffd40b9a821dbb49
matheus silva <matheus23rs@gmail.com>
2022-03-27 12:40:09 -0300

    estrategia ataque e defesa

--- src/attacker.c
+++ src/attacker.c
@@ -1,5 +1,6 @@
 // Standard headers
 #include <stdio.h>
+#include <stdlib.h>
 
 // Internal headers
 #include "direction.h"
@@ -21,9 +22,36 @@ direction_t execute_attacker_strategy(
   // TODO: unused parameters, remove these lines later
   UNUSED(attacker_position);
   UNUSED(defender_spy);
-
-  // TODO: Implement Attacker logic here
-  return (direction_t) DIR_RIGHT;
+  int random = rand() % 100;
+  if (random >= 0 && random < 6) {
+    return (direction_t) DIR_STAY;
+  }
+  else if (random >= 6 && random < 16) {
+    return (direction_t) DIR_UP;
+  }
+  else if (random >= 16 && random < 18) {
+    return (direction_t) DIR_UP_LEFT;
+  }
+  else if (random >= 18 && random < 20) {
+    return (direction_t) DIR_DOWN_LEFT;
+  }
+  else if (random >= 20 && random < 30) {
+    return (direction_t) DIR_DOWN;
+  }
+  else if (random >= 30 && random < 40) {
+    return (direction_t) DIR_LEFT;
+  }
+  else if (random >= 40 && random < 50) {
+    return (direction_t) DIR_RIGHT;
+  }
+  else if (random >= 50 && random < 75) {
+    return (direction_t) DIR_UP_RIGHT;
+  }
+  else if (random >= 75 && random < 100) {
+    return (direction_t) DIR_DOWN_RIGHT;
+  }
+  return (direction_t) DIR_DOWN_RIGHT;
 }
+
 
 /*----------------------------------------------------------------------------*/
--- src/defender.c
+++ src/defender.c
@@ -21,8 +21,34 @@ direction_t execute_defender_strategy(
   // TODO: unused parameters, remove these lines later
   UNUSED(defender_position);
   UNUSED(attacker_spy);
-
-  // TODO: Implement Defender logic here
+  int random = rand() % 100;
+  if (random >= 0 && random < 20) {
+    return (direction_t) DIR_STAY;
+  }
+  else if (random >= 20 && random < 40) {
+    return (direction_t) DIR_UP;
+  }
+  else if (random >= 40 && random < 60) {
+    return (direction_t) DIR_DOWN;
+  }
+  else if (random >= 60 && random < 66) {
+    return (direction_t) DIR_UP_LEFT;
+  }
+  else if (random >= 66 && random < 72) {
+    return (direction_t) DIR_DOWN_LEFT;
+  }
+  else if (random >= 72 && random < 78) {
+    return (direction_t) DIR_LEFT;
+  }
+  else if (random >= 78 && random < 84) {
+    return (direction_t) DIR_RIGHT;
+  }
+  else if (random >= 84 && random < 90) {
+    return (direction_t) DIR_UP_RIGHT;
+  }
+  else if (random >= 90 && random < 100) {
+    return (direction_t) DIR_DOWN_RIGHT;
+  }
   return (direction_t) DIR_LEFT;
 }
 
